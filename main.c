c6d27cb806cc045598dbd394c9160b193eadd710
Stephatt <ttt1089@126.com>
2014-01-08 21:04:26 +0800

    修改了画飞机函数和猜位置函数，添加计算机随机画飞机、猜数
    修改了画飞机函数，用数组作为实参；但由于计算机随机产生飞机，位置不符合要求。

--- main.c
+++ main.c
@@ -15,19 +15,38 @@ int humanPlayer[10][10]={0};
 
 int headComputer;
 int headPlayer;
+int playerGuess;
+int computerGuess;
+
 
 void main()
 {
     void outputmap(int line);
     outputmap(10);
-    void planePlayer(int head);
+    
+    void planePlayer(int head,int player[10][10]);
     printf("input your plane's head is:");
     scanf("%d",&headPlayer);
-    planePlayer(headPlayer);
-    //headComputer=rand()%100;
-    //planePlayer(headComputer,computerPlayer[10][10]);
+    planePlayer(headPlayer,humanPlayer);
+    srand((unsigned long)time(NULL));
+    headComputer=rand()%100;
+    planePlayer(headComputer,computerPlayer);
     
-    
+    int AbattlePlane(int guess);
+    int BbattlePlane(int guess);
+    srand((unsigned long)time(NULL));
+    computerGuess=rand()%100;
+    for (int i=1,j=1;;) {
+        printf("Your turn to guess where is the plane:");
+        scanf("%d",&playerGuess);
+        i=AbattlePlane(playerGuess);
+        j=BbattlePlane(computerGuess);
+        if (i==0||j==0) {
+            break;
+        }
+    }
+        
+        
 }
 
 /*给出图纸坐标*/
@@ -46,25 +65,25 @@ void outputmap(int line)
 }
 
 /*画飞机*/
-void planePlayer(int head)
+void planePlayer(int head,int player[10][10])
 {
-    humanPlayer[(int)(head/10)][head%10-1]=0;
+    player[(int)(head/10)][head%10-1]=0;
     int angel;
     printf("choose the angel of your plane,0,90,180 or 270:");
     scanf("%d",&angel);
     switch (angel) {
         case 0:
             for (int i=1; i<=4; i++) {
-                humanPlayer[(int)(head/10)+i][head%10-1]=0;
+                player[(int)(head/10)+i][head%10-1]=0;
                 if (i==1) {
                     for (int j=1; j<=2; j++) {
-                        humanPlayer[(int)(head/10)+i][head%10-1-j]=0;
-                        humanPlayer[(int)(head/10)+i][head%10-1+j]=0;
+                        player[(int)(head/10)+i][head%10-1-j]=0;
+                        player[(int)(head/10)+i][head%10-1+j]=0;
                     }
                 }
                 if (i==4) {
-                    humanPlayer[(int)(head/10)+i][head%10-1-1]=0;
-                    humanPlayer[(int)(head/10)+i][head%10-1+1]=0;
+                    player[(int)(head/10)+i][head%10-1-1]=0;
+                    player[(int)(head/10)+i][head%10-1+1]=0;
                 }
             }
             
@@ -72,16 +91,16 @@ void planePlayer(int head)
             break;
         case 90:
             for (int i=1; i<=4; i++) {
-                humanPlayer[(int)(head/10)][head%10-1-i]=0;
+                player[(int)(head/10)][head%10-1-i]=0;
                 if (i==1) {
                     for (int j=1; j<=2; j++) {
-                        humanPlayer[(int)(head/10)-j][head%10-1-i]=0;
-                        humanPlayer[(int)(head/10)+j][head%10-1-i]=0;
+                        player[(int)(head/10)-j][head%10-1-i]=0;
+                        player[(int)(head/10)+j][head%10-1-i]=0;
                     }
                 }
                 if (i==4) {
-                    humanPlayer[(int)(head/10)-1][head%10-1-i]=0;
-                    humanPlayer[(int)(head/10)+1][head%10-1-i]=0;
+                    player[(int)(head/10)-1][head%10-1-i]=0;
+                    player[(int)(head/10)+1][head%10-1-i]=0;
                 }
             }
             
@@ -89,16 +108,16 @@ void planePlayer(int head)
             break;
         case 180:
             for (int i=1; i<=4; i++) {
-                humanPlayer[(int)(head/10)+i][head%10-1]=0;
+                player[(int)(head/10)+i][head%10-1]=0;
                 if (i==1) {
                     for (int j=1; j<=2; j++) {
-                        humanPlayer[(int)(head/10)-i][head%10-1-j]=0;
-                        humanPlayer[(int)(head/10)-i][head%10-1+j]=0;
+                        player[(int)(head/10)-i][head%10-1-j]=0;
+                        player[(int)(head/10)-i][head%10-1+j]=0;
                     }
                 }
                 if (i==4) {
-                    humanPlayer[(int)(head/10)-i][head%10-1-1]=0;
-                    humanPlayer[(int)(head/10)-i][head%10-1+1]=0;
+                    player[(int)(head/10)-i][head%10-1-1]=0;
+                    player[(int)(head/10)-i][head%10-1+1]=0;
                 }
             }
             
@@ -106,16 +125,16 @@ void planePlayer(int head)
             break;
         case 270:
             for (int i=1; i<=4; i++) {
-                humanPlayer[(int)(head/10)][head%10-1+i]=0;
+                player[(int)(head/10)][head%10-1+i]=0;
                 if (i==1) {
                     for (int j=1; j<=2; j++) {
-                        humanPlayer[(int)(head/10)-j][head%10-1+i]=0;
-                        humanPlayer[(int)(head/10)+j][head%10-1+i]=0;
+                        player[(int)(head/10)-j][head%10-1+i]=0;
+                        player[(int)(head/10)+j][head%10-1+i]=0;
                     }
                 }
                 if (i==4) {
-                    humanPlayer[(int)(head/10)-1][head%10-1+i]=0;
-                    humanPlayer[(int)(head/10)+1][head%10-1+i]=0;
+                    player[(int)(head/10)-1][head%10-1+i]=0;
+                    player[(int)(head/10)+1][head%10-1+i]=0;
                 }
             }
             
@@ -127,9 +146,50 @@ void planePlayer(int head)
     }
     for (int i=0; i<10; i++) {
         for (int j=0; j<10; j++) {
-            printf("%d",humanPlayer[i][j]);
+            printf("%d",player[i][j]);
             printf("\t");
         }
         printf("\n");
     }
 }
+
+/*用户猜数打飞机*/
+int AbattlePlane(int guess)
+{
+    if (computerPlayer[(int)(guess/10)][guess%10-1]==0&computerPlayer[(int)(guess/10)-1][guess%10-1]!=0&computerPlayer[(int)(guess/10)][guess%10+1]!=0) {
+        printf("RIGHT");
+        printf("\n");
+        return 0;
+    }
+    else if (computerPlayer[(int)(guess/10)][guess%10-1]==0){
+        printf("NEARLY");
+        printf("\n");
+        return 1;
+    }
+    else if (computerPlayer[(int)(guess/10)][guess%10-1]!=0){
+        printf("BAD");
+        printf("\n");
+        return 1;
+    }
+}
+
+/*电脑猜飞机*/
+int BbattlePlane(int guess)
+{
+    if (humanPlayer[(int)(guess/10)][guess%10-1]==0&humanPlayer[(int)(guess/10)-1][guess%10-1]!=0&humanPlayer[(int)(guess/10)][guess%10]!=0) {
+        printf("RIGHT");
+        printf("\n");
+        return 0;
+    }
+    else if (humanPlayer[(int)(guess/10)][guess%10-1]==0){
+        printf("NEARLY");
+        printf("\n");
+        return 1;
+    }
+    else if (humanPlayer[(int)(guess/10)][guess%10-1]!=0){
+        printf("BAD");
+        printf("\n");
+        return 1;
+    }
+}
+
